561f2c4468f70ee3d261df3e86f0db4993b060bc
ysc <yang-shangchuan@qq.com>
2015-04-13 21:13:32 +0800

    改用word分词1.3

--- deep-qa/src/main/java/org/apdplat/qa/score/evidence/SkipBigramEvidenceScore.java
+++ deep-qa/src/main/java/org/apdplat/qa/score/evidence/SkipBigramEvidenceScore.java
@@ -50,7 +50,7 @@ public class SkipBigramEvidenceScore implements EvidenceScore {
         LOG.debug("*************************");
         LOG.debug("Evidence 跳跃二元模型评分开始");
         //1、对问题进行分词
-        List<String> questionTerms = question.getTerms();
+        List<String> questionTerms = question.getWords();
         //2、利用跳跃二元模型构造出问题的所有跳跃二元表达式
         List<String> patterns = new ArrayList<>();
         for (int i = 0; i < questionTerms.size() - 2; i++) {
