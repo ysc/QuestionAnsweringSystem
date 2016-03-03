f1c23fed8ba0ef2699daea3edb666047a658c818
ysc <yang.shangchuan@kuyun.com>
2016-03-03 13:05:15 +0800

    fix format

--- deep-qa/src/main/java/org/apdplat/qa/questiontypeanalysis/patternbased/PatternBasedMultiLevelQuestionClassifier.java
+++ deep-qa/src/main/java/org/apdplat/qa/questiontypeanalysis/patternbased/PatternBasedMultiLevelQuestionClassifier.java
@@ -53,7 +53,7 @@ import org.slf4j.LoggerFactory;
  * 2、和问题分词及词性标注之后的【词和词性序列】进行匹配，如：apdplat/en 的/uj 发起人/n 是/v 谁/RW.RWPersonSingle ？/w
  * 3、和问题分词及词性标注之后的【词性序列】进行匹配，如：en/uj/n/v/RW.RWPersonSingle/w
  * 4、利用问题的【主谓宾 词 和 词性】进行匹配，如：发起人/n 是/v 谁/RW.RWPersonSingles
- * 5、利用问题的【主谓宾词性】进行匹配，如：n/v/RW.RWPersonSingle * @author 杨尚川
+ * 5、利用问题的【主谓宾词性】进行匹配，如：n/v/RW.RWPersonSingle
  * 
  * @author 杨尚川
  */
